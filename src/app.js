0934bc9d355307fb1eb16e4b6ed0af8fb348cbda
Christian Junker <chjdev@gmail.com>
2017-08-22 14:08:15 +0200

    uppercase

--- src/app.js
+++ src/app.js
@@ -15,4 +15,4 @@ rootDialog(bot);
 
 const server = restify.createServer();
 server.post('/v4/eev/api/messages', connector.listen());
-server.listen(process.env.port || 3978, () => console.log('%s listening to %s', server.name, server.url));
+server.listen(process.env.PORT || 3978, () => console.log('%s listening to %s', server.name, server.url));
