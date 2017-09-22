e24f5d447935595dab8eedadd5055c15f29e52ee
Christian Junker <chjdev@gmail.com>
2017-09-22 12:45:20 +0200

    move configs out of db into env
    fixes

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,23 @@
 /**
  * Created by chris on 19/01/2017.
  */
-if (process.env.DEV_MODE === 'true') {
+import config from "../config";
+
+if (config.devMode === true) {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 }
 
 import Swagger from "swagger-client";
 
 const client = (swagger) => new Swagger({
-  url: `${process.env.DOMAIN}${swagger}`,
+  url: `${config.api.basePath}${swagger}`,
   usePromise: true,
   authorizations: {
-    headerAuth: new Swagger.ApiKeyAuthorization('Authorization-Token', process.env.API_KEY, 'header')
     // TODO use session.userData.jwt
+//    headerAuth: new Swagger.ApiKeyAuthorization('Authorization', process.env.API_KEY, 'header')
   }
 });
 
-export const activities = client('/v4/activities/swagger.json');
+export const activities = client('/activities/swagger.json');
 
-export const model = client('/v4/model/swagger.json');
+export const model = client('/model/swagger.json');
